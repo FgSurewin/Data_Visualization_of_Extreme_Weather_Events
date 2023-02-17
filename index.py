3e7ef0ee4f3ee1dd806cfe60a7a7fbb79e50b8e4
FgSurewin <mrliu3244794@gmail.com>
2023-02-16 19:45:06 -0500

    fix bugs -v1

--- index.py
+++ index.py
@@ -8,22 +8,22 @@ from dash.dependencies import Input, Output
 
 # --------------------------------- old code --------------------------------- #
 # Connect to main app.py file
-# from app import app
-# from app import server
+from app import app
+from app import server
 
 # meta_tags are required for the app layout to be mobile responsive
-app = dash.Dash(__name__, suppress_callback_exceptions=True,
-                meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0'}]
-                )
+# app = dash.Dash(__name__, suppress_callback_exceptions=True,
+#                 meta_tags=[{'name': 'viewport',
+#                             'content': 'width=device-width, initial-scale=1.0'}]
+                # )
 # (1) Style:
 # Bootstrap Theme: https://bootswatch.com/
 # Download the one you want and put it inside the assets folder. (Or replace the old css file)
 
 # (2) Title:
-app.title = "Data Analysis of Extreme Weather Events"
+# app.title = "Data Analysis of Extreme Weather Events"
 
-server = app.server
+# server = app.server
 
 
 # Connect to your app pages
